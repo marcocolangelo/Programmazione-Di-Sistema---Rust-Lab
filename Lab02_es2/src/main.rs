3ff5748ce2d907738481ea00dd517dade78856ec
MarcoCuda <83675154+marcocolangelo@users.noreply.github.com>
2023-05-07 17:57:27 +0200

    Some problems with .split('/') in mk_dir

--- Lab02_es2/src/main.rs
+++ Lab02_es2/src/main.rs
@@ -3,16 +3,17 @@ use std::{io, ops::Index, time};
 
 
 use anyhow::Result;
-
+#[derive(Debug)]
 pub enum FileType{
     Text,Binary
 }
 
+#[derive(Debug)]
 enum Node {
     File(File),
     Dir(Dir)
 }
-
+#[derive(Debug)]
 struct File{
     name : String,
     content: Vec<u8>,
@@ -20,6 +21,7 @@ struct File{
     type_:FileType
 }
 
+#[derive(Debug)]
 struct Dir{
     name  : String,
     creation_time : u64,
@@ -53,19 +55,19 @@ impl FileSystem{
         return  FileSystem {root : root_dir};
     }
 
-    fn search_parent(self,path : Vec<&str>) -> Option<&Dir>{
+    fn search_parent(&mut self,path : Vec<&str>) -> Option<&mut Dir>{
         let mut found=0;
-        let mut curr_dir = &(self.root);
+        let mut curr_dir:&mut Dir = &mut (self.root);
         let mut index = 0;
-
+        let mut j = 0;
         //cerchiamo lungo tutte le directory del path intero
         let p_len = path.len();
         for dir in path {
             found = 0;
             //se siamo arrivati alla fine del path ritorniamo la directory corrente
             if index == (p_len - 1) {
-                let result = &(*curr_dir);
-                return Some(result);
+                
+                return Some(curr_dir);
             }
             //se siamo invece all'inizio del path e questo non inizia con D:/ allora ritorna errore
            if dir != curr_dir.name && index== 0 {
@@ -77,6 +79,8 @@ impl FileSystem{
                 continue;
             }else{ //se invece NON siamo all'inizio del path
                     //leggiamo lungo i figli della directory corrente
+                    j=0;
+                    found=0;
                     for child in &(curr_dir.children) {
                         match child {
                             //se il figlio correntemente in analisi è una directory allora leggila
@@ -84,8 +88,9 @@ impl FileSystem{
                                                     //se il suo nome coincide con il nome della variabile dir (attuale sottoparte del path in analisi)
                                                     if c.name == dir {
                                                         //allora spostati alla cartella in questione e segnalalo con un flag
-                                                        curr_dir = c;
+                                                       
                                                         found = 1;
+                                                        
                                                         break;
                                                     }
                                                 },
@@ -94,12 +99,19 @@ impl FileSystem{
                                                     continue;
                             }
                         }
+
+                        j+=1;
                         
                     }
                     //se lo studio lungo il vettore di directory non ha portato a nulla allora ritorna errore
                     if found == 0 {
                         println!("Directory non trovata in search_parent");
                         return None;
+                    }else{
+                        match &mut curr_dir.children[j] {
+                            Node::Dir(c) => curr_dir = c,
+                            Node::File(_) => return None
+                        }
                     }
                 
                 index+=1;
@@ -109,15 +121,18 @@ impl FileSystem{
         return None;
     }
 
-    fn mk_dir(self,path : &str) -> Result<&mut Dir>{
-        let mut directories : Vec<&str> = path.split('/').collect();
-        directories = directories[1..directories.len()-1].to_vec();
+    fn mk_dir(&mut self,path : &str) -> Result<&mut Dir>{
+        println!("{:?}",path);
+        //PERCHE' NON FUNZIONAAAAAAA
+        let dire : Vec<&str> = path.split('/').collect();
+        let path_len = dire.len();
+        let directories = dire[0..path_len-1].to_vec();
         let dir_name = directories[directories.len()-1];
         //ad es D:/a/b/c viene divisa in 'D:' ,'a' ,'b' e 'c'
         //usa una funzione di ricerca che partendo da FileSystem -> root vada a cercare lungo i figli di D: la cartella 'a', lungo i figli di 'a' cerca 'b' e così via
         
         //trova il direttorio padre
-        let parent_dir:Option<&Dir> = self.search_parent(directories);
+        let parent_dir = self.search_parent(directories);
 
         match parent_dir {
             None  => return Err(anyhow::Error::new(io::Error::new(io::ErrorKind::NotFound, "Directory non trovata in mk_dir"))),
@@ -125,9 +140,9 @@ impl FileSystem{
                                         let dir = Dir::new(dir_name);
 
                                         parent_dir.children.push(Node::Dir(dir));
-
+                                        let len = parent_dir.children.len();
                                         //faccio tutto sto macello perchè voglio ritornare un puntatore alla zona di memoria del vettore non alla variabile nello scope corrente
-                                        match &mut parent_dir.children[parent_dir.children.len() - 1] {
+                                        match &mut parent_dir.children[len - 1] {
                                             Node::Dir(dir) => return Ok(dir),
                                             _ => return Err(anyhow::Error::new(io::Error::new(io::ErrorKind::Other, "Il Node ricercato non è una directory"))),
                                         }
@@ -138,16 +153,16 @@ impl FileSystem{
         
     }
 
-    fn rm_dir(self,path:&str) -> anyhow::Result<&Dir>{
+    fn rm_dir(&mut self,path:&str) -> anyhow::Result<&Dir>{
         let mut directories : Vec<&str> = path.split('/').collect();
         directories = directories[1..directories.len()-1].to_vec();
         let dir_name = directories[directories.len()-1];
 
         //trova il direttorio padre
-        let parent_dir:&Dir = self.search_parent(directories).expect("Impossibile trovare il direttorio padre in rm_dir");
+        let parent_dir:&mut Dir = self.search_parent(directories).expect("Impossibile trovare il direttorio padre in rm_dir");
         let mut index = 0;
 
-        for child in parent_dir.children {
+        for child in &parent_dir.children {
 
             match child{
                 Node::Dir(dir) => {
@@ -165,7 +180,7 @@ impl FileSystem{
 
     }
 
-    fn new_file(self,path: &str, file: File) -> Result<&mut File> {
+    fn new_file(&mut self,path: &str, file: File) -> Result<&mut File> {
         let mut directories : Vec<&str> = path.split('/').collect();
         directories = directories[1..directories.len()-1].to_vec();
         let file_name = directories[directories.len()-1];
@@ -173,7 +188,7 @@ impl FileSystem{
         //usa una funzione di ricerca che partendo da FileSystem -> root vada a cercare lungo i figli di D: la cartella 'a', lungo i figli di 'a' cerca 'b' e così via
         
         //trova il direttorio padre
-        let parent_dir:Option<&Dir> = self.search_parent(directories);
+        let parent_dir:Option<&mut Dir> = self.search_parent(directories);
 
         match parent_dir {
             None  => return Err(anyhow::Error::new(io::Error::new(io::ErrorKind::NotFound, "Directory non trovata in new_file"))),
@@ -181,9 +196,9 @@ impl FileSystem{
                                         
 
                                         parent_dir.children.push(Node::File(file));
-
+                                        let len = parent_dir.children.len();
                                         //faccio tutto sto macello perchè voglio ritornare un puntatore alla zona di memoria del vettore non alla variabile nello scope corrente
-                                        match &mut parent_dir.children[parent_dir.children.len() - 1] {
+                                        match &mut parent_dir.children[len - 1] {
                                             Node::File(file) => return Ok(file),
                                             _ => return Err(anyhow::Error::new(io::Error::new(io::ErrorKind::Other, "Il node ricercato in new_file non è un file"))),
                                         }
@@ -193,7 +208,7 @@ impl FileSystem{
         
     }
 
-    fn rm_file(self,path:&str) -> anyhow::Result<&Dir>{
+    fn rm_file(&mut self,path:&str) -> anyhow::Result<&Dir>{
         let mut directories : Vec<&str> = path.split('/').collect();
         directories = directories[1..directories.len()-1].to_vec();
         let file_name = directories[directories.len()-1];
@@ -238,5 +253,11 @@ impl FileSystem{
 
 
 fn main() {
-    println!("Hello, world!");
+    let mut fs = FileSystem::new();
+    let mut new_dir = fs.mk_dir("D/first").unwrap();
+    let path = "D:/first/second";
+    let v : Vec<&str>= path.split('/').collect(); 
+    println!("{:?}",v);
+    new_dir = fs.mk_dir(path).unwrap();
+    
 }
